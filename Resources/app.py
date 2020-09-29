384c1d5a2ffed2e7b7c66353e4ad943d874ae24f
Asia-Hewett <asia.hewett0046@gmail.com>
2020-09-29 16:03:50 -0500

    Adding Flask Queries for Stations/Tobs

--- Resources/app.py
+++ Resources/app.py
@@ -17,7 +17,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return (
-    f"Welcome to the Justice League API!<br/>"
     f"Available Routes:<br/>"
     f"/api/v1.0/precipitation<br/>"
     f"/api/v1.0/stations<br/>"
@@ -33,21 +32,36 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
 
-    all_measurments = []
+    all_measurements = []
+    
     for date, prcp in results:
         prcp_dict = {}
         prcp_dict[date] = prcp
-        all_measurments.append(all_measurments)
-    return jsonify(all_measurments)
+        all_measurements.append(prcp_dict)
+    return jsonify(all_measurements)
 
-# We are currently setting up the endpoints
-# About and Home are the endpoints
-# When a user makes a "get" request they will be make calls to your endpoints
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Station.station, Station.name).all()
+    session.close()
 
-# This is where the actual action occurs in a Python page
-# We're going to take the app that we've created above
-# and run it down here
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(func.strftime(Measurement.date) > "2016-08-23").\
+            filter(Measurement.station == 'USC00519281').all()
+    session.close()
+
+    annual_tobs = list(np.ravel(results))
+    return jsonify(annual_tobs)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
